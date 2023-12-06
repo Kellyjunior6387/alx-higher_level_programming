c9aab0bf5699b05dcb35207805f5c485855e076a
Kellyjunior6387 <kellyjunior6386@gmail.com>
2023-12-06 19:14:09 +0300

    more dsa

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new = [[0] * len(row) for row in matrix]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            new[i][j] = new[i][j] ** 2
+    return new
