333480faa2c9ffb5d39e8583555d326b041ffc08
Nyams <129679320+Kellyjunior6387@users.noreply.github.com>
2024-03-05 15:13:24 +0300

    sql

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,2 @@
+-- CREATES force_name on MYSQL server
+CREATE TABLE IF NOT EXISTS `force_name`(`id` INT, `name` VARCHAR(256) NOT NULL);
---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- Creates database hbtn_0d_usa and tables states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY ,name VARCHAR(256));
