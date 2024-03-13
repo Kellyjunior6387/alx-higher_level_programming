1d0cc53dfd5ec7fda3729084584f9653e8e1e386
Nyams <129679320+Kellyjunior6387@users.noreply.github.com>
2024-03-14 01:17:55 +0300

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,19 @@
-const Square2 = require('./5-square')
+#!/usr/bin/node
+const SquareP = require('./5-square');
 
-class Square extends Square2 {
-	charPrint(c) {
-		if (c == undefined)
-			c = 'X';
-		let row = c.repeat(this.width);
-		for (let i = 0; i < this.height; i++) {
-			console.log(row);
-		}
-	}
+class Square extends SquareP {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
 }
+
+module.exports = Square;
