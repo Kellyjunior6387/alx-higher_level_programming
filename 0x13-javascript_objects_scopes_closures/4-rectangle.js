26cded95b58b80011e19bc22bc6a0afce119d1b1
Nyams <129679320+Kellyjunior6387@users.noreply.github.com>
2024-03-14 01:16:18 +0300

    Update 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,31 +1,32 @@
 #!/usr/bin/node
 class Rectangle {
-	constructor(w, h) {
-		if ( w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-			return {};
-		} else {
-			this.width = w;
-			this.height = h;
-		}
-	}
-	print() {
-	for (let i = 0; i < this.height; i++) {
-		let row = '';
-		for (let j = 0; j < this.width; j++) {
-			row += 'X'
-		}
-		console.log(row);
-	}
-	}
-	rotate() {
-		let temp = this.width;
-		this.width = this.height;
-		this.height = this.width;
-	}
-	double() {
-		this.width = this.width * 2;
-		this.height = this.height * 2;
-	}
-}
-module.exports = Rectangle;
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
+  }
+
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
