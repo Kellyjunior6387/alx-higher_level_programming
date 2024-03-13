17c9894076809e39d676e70b3bc4b6b518d066e4
Nyams <129679320+Kellyjunior6387@users.noreply.github.com>
2024-03-14 01:18:56 +0300

    Update 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-
- exports.nbOccurences = function (list, searchElement) {
-	 let count = 0;
-	 for (let element in list){
-		 if (element === searchElement){
-			 count++;
-		 }
-	 }
-	 return count;
- };
+exports.nbOccurences = function (list, searchElement) {
+  let nOccurrences = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      nOccurrences++;
+    }
+  }
+  return nOccurrences;
+};
