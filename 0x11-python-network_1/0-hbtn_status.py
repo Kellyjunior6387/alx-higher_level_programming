0a01a9ec830a89613eab96c7df76e5bae14d0e66
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-03-29 04:11:35 +0300

    requests

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """get https://alx-intranet.hbtn.io/status"""
-from urllib import request
+from urllib import request, error
+
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-
-    with request.urlopen(url) as response:
-        body = response.read().decode('utf-8')
-
-    print("Body response:")
-    print("\t- type:", type(body))
-    print("\t- content:", body)
-     print("\t- utf8 content: {}".format(response.decode('utf-8')))
+    try:
+        with request.urlopen(url) as response:
+            response = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(response)))
+            print("\t- content: {}".format(response))
+            print("\t- utf8 content: {}".format(response.decode('utf-8')))
+    except error.URLError:
+        print("Cannot connect to https://alx-intranet.hbtn.io/status")
