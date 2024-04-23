ade9cc37dc4c6037e3b96909fa1460622eaf0f1c
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-04-24 00:26:23 +0300

    js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+// Read a file
 
 const fs = require('fs')
 file_path = process.argv[2]
@@ -9,4 +10,4 @@ fs.readFile(file_path, 'utf-8', (error, data) => {
 	} else {
 		console.log(data);
 	}
-})
+});
